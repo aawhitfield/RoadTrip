3c9da0dd889c921003adcb077cd91befed42d1b5
Aaron Whitfield <aawhitfield@hotmail.com>
2018-01-14 22:23:31 -0800

    upload 3

--- app/src/main/java/com/infinitylabs/roadtrip/roadtrip/BattleshipActivity.java
+++ app/src/main/java/com/infinitylabs/roadtrip/roadtrip/BattleshipActivity.java
@@ -2,17 +2,12 @@ package com.infinitylabs.roadtrip.roadtrip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
-public class MainActivity extends AppCompatActivity {
+public class BattleshipActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_battleship);
     }
-
-
 }
