6eec969a149a8b3d0d3cc65fd8bf5b7d93772d3a
Aaron Whitfield <aawhitfield@hotmail.com>
2018-01-25 21:36:43 -0800

    Added test features to buttons and tested toasts.

--- app/src/main/java/com/infinitylabs/roadtrip/roadtrip/SpotTheCarActivity.java
+++ app/src/main/java/com/infinitylabs/roadtrip/roadtrip/SpotTheCarActivity.java
@@ -2,8 +2,16 @@ package com.infinitylabs.roadtrip.roadtrip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SpotTheCarActivity extends AppCompatActivity {
+
+    public void sankShip(View view)
+    {
+         Toast.makeText(this, "You sank my Battleship", Toast.LENGTH_LONG).show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
